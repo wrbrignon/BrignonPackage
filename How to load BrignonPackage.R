3340b11b3f040a90967f7762d1bf08a79f1154a5
W.R. Brignon <william.r.brignon@gmail.com>
2017-02-16 14:02:17 -0800

    Add files via upload

---
+++ How to load BrignonPackage.R
@@ -0,0 +1,15 @@
+##install packages/libraries required to load BrignonPackage
+install.packages(c("devtools","httr"))
+library(devtools)
+library(httr)
+
+##work around for certificate error
+httr::set_config( config( ssl_verifypeer = 0L ) )
+
+##install and load BrignonPackage
+install_github("wrbrignon/BrignonPackage")
+library(BrignonPackage)
+
+##test if it worked
+mom("beta",0.5,0.2)   ##get beta dist parms
+?ipak  ##show ipak help document
